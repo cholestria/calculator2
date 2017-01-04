3b241e09a6d2f23b47cc18714c715b58dafc83d1
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-04 15:18:12 -0800

    Working 2nd calculator exercise

---
+++ calculator.py
@@ -0,0 +1,42 @@
+"""
+calculator.py
+
+Using our arithmetic.py file from Exercise02, create the
+calculator program yourself in this file.
+"""
+
+from arithmetic import *
+
+
+# Your code goes here
+def main():
+    while True:
+        user_input = raw_input("> ")
+        tokens = user_input.split()
+        if tokens[0] == 'q':
+            quit()
+        elif len(tokens) < 3:
+            print "I don't understand"
+        elif tokens[0] == '+':
+            print add(int(tokens[1]), int(tokens[2]))
+        elif tokens[0] == '-':
+            print subtract(int(tokens[1]), int(tokens[2]))
+        elif tokens[0] == '*':
+            print multiply(int(tokens[1]), int(tokens[2]))
+        elif tokens[0] == '/':
+            print divide(int(tokens[1]), int(tokens[2]))
+        elif tokens[0] == 'square':
+            print square(int(tokens[1]), int(tokens[2]))
+        elif tokens[0] == 'cube':
+            print cube(int(tokens[1]), int(tokens[2]))
+        elif tokens[0] == 'pow' or tokens[0] == '**' or tokens[0] == 'exp':
+            print power(int(tokens[1]), int(tokens[2]))
+        elif tokens[0] == 'mod' or tokens[0] == '%':
+            print mod(int(tokens[1]), int(tokens[2]))
+        else:
+            print "I don't understand"
+
+
+
+if __name__ == '__main__':
+    main()
