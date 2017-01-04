643c4fddfb78bf9ed40cc9db29f7cb7edb438cf0
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-04 15:39:05 -0800

    working calulator before further study

--- calculator.py
+++ calculator.py
@@ -13,30 +13,29 @@ def main():
     while True:
         user_input = raw_input("> ")
         tokens = user_input.split()
-        if tokens[0] == 'q':
-            quit()
-        elif len(tokens) < 3:
-            print "I don't understand"
-        elif tokens[0] == '+':
-            print add(int(tokens[1]), int(tokens[2]))
-        elif tokens[0] == '-':
-            print subtract(int(tokens[1]), int(tokens[2]))
-        elif tokens[0] == '*':
-            print multiply(int(tokens[1]), int(tokens[2]))
-        elif tokens[0] == '/':
-            print divide(int(tokens[1]), int(tokens[2]))
-        elif tokens[0] == 'square':
-            print square(int(tokens[1]), int(tokens[2]))
-        elif tokens[0] == 'cube':
-            print cube(int(tokens[1]), int(tokens[2]))
-        elif tokens[0] == 'pow' or tokens[0] == '**' or tokens[0] == 'exp':
-            print power(int(tokens[1]), int(tokens[2]))
-        elif tokens[0] == 'mod' or tokens[0] == '%':
-            print mod(int(tokens[1]), int(tokens[2]))
-        else:
-            print "I don't understand"
-
-
+        try:
+            if tokens[0] == 'q':
+                quit()
+            elif tokens[0] == '+':
+                print add(int(tokens[1]), int(tokens[2]))
+            elif tokens[0] == '-':
+                print subtract(int(tokens[1]), int(tokens[2]))
+            elif tokens[0] == '*':
+                print multiply(int(tokens[1]), int(tokens[2]))
+            elif tokens[0] == '/':
+                print divide(int(tokens[1]), int(tokens[2]))
+            elif tokens[0] == 'square':
+                print square(int(tokens[1]))
+            elif tokens[0] == 'cube':
+                print cube(int(tokens[1]))
+            elif tokens[0] == 'pow' or tokens[0] == '**' or tokens[0] == 'exp':
+                print power(int(tokens[1]), int(tokens[2]))
+            elif tokens[0] == 'mod' or tokens[0] == '%':
+                print mod(int(tokens[1]), int(tokens[2]))
+            else:
+                print "I don't understand"
+        except:
+            print "This doesn't work"        
 
 if __name__ == '__main__':
     main()
