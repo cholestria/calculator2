5ee5aef892927855ffac3446b83b94e0ed164bfa
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-04 16:04:53 -0800

    using old arithmetic for new calculator

--- arithmetic.py
+++ arithmetic.py
@@ -1,40 +1,38 @@
-def add(numstrings):
-    if len(numstrings) == 0:
-        return None
-    else:
-        total = 0
-        for s in numstrings:
-            total += int(s)
-        return total
+def add(num1, num2):
+    """Return the sum of two numbers"""
+    return num1 + num2
 
 
 def subtract(num1, num2):
+    """Return the difference of two numbers"""
     return num1 - num2
 
 
 def multiply(num1, num2):
+    """Return the product of two numbers"""
     return num1 * num2
 
 
 def divide(num1, num2):
-    # Need to turn at least argument to float for division to
-    # not be integer division
-    return float(num1) / float(num2) 
+    """Return the quotient of two numbers as a float"""
+    return float(num1) / num2
 
 
-def square(num1):
-    # Needs only one argument
-    return num1 * num1
+def square(num):
+    """Return the square of a number"""
+    return num ** 2
 
 
-def cube(num1):
-    # Needs only one argument
-    return num1 * num1 * num1
+def cube(num):
+    """Return the cube of a number"""
+    return num ** 3
 
 
-def power(num1, num2):
-    return num1 ** num2  # ** = exponent operator
+def power(num, exponent):
+    """Return num raised to the power of exponent"""
+    return num ** exponent
 
 
 def mod(num1, num2):
+    """Return remainder of num1 divided by num2"""
     return num1 % num2
