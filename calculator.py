d36d1809a9e619350e03ef78455e6fa0ed990f15
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-05 12:19:26 -0800

    working input from file

--- calculator.py
+++ calculator.py
@@ -8,36 +8,54 @@ calculator program yourself in this file.
 from arithmetic import *
 
 
+def file_to_print(in_filename):
+    incoming_file = open(in_filename)
+    for line in incoming_file:
+        print line.strip(), "=", calculate_from_line(line)
+
+
+def file_to_file(in_filename, out_filename):
+    incoming_file = open(in_filename)
+
+    incoming_file.close()
+
+
+def calculate_from_line(line):
+    tokens = line.split()
+    try:
+        if tokens[0] == 'q':
+            return 'quit'
+        else:
+            numbers = map(int, tokens[1:])
+            if tokens[0] == '+':
+                return "%.2f" % reduce(add, numbers)
+            elif tokens[0] == '-':
+                return '{:.2f}'.format(reduce(subtract, numbers))
+            elif tokens[0] == '*':
+                return "{:.2f}".format(reduce(multiply, numbers))
+            elif tokens[0] == '/':
+                return "{:06.3f}".format(reduce(divide, numbers))
+            elif tokens[0] == 'square':
+                return square(int(tokens[1]))
+            elif tokens[0] == 'cube':
+                return cube(int(tokens[1]))
+            elif tokens[0] == 'pow' or tokens[0] == '**' or tokens[0] == 'exp':
+                return reduce(power, numbers)
+            elif tokens[0] == 'mod' or tokens[0] == '%':
+                return reduce(mod, numbers)
+            else:
+                return "I don't understand"
+    except:
+        return "This doesn't work"
+
+
 # Your code goes here
 def main():
     while True:
         user_input = raw_input("> ")
-        tokens = user_input.split()
-        try:
-            if tokens[0] == 'q':
-                quit()
-            else:
-                numbers = map(int, tokens[1:])
-                if tokens[0] == '+':
-                    print reduce(add, numbers)
-                elif tokens[0] == '-':
-                    print reduce(subtract, numbers)
-                elif tokens[0] == '*':
-                    print reduce(multiply, numbers)
-                elif tokens[0] == '/':
-                    print reduce(divide, numbers)
-                elif tokens[0] == 'square':
-                    print square(int(tokens[1]))
-                elif tokens[0] == 'cube':
-                    print cube(int(tokens[1]))
-                elif tokens[0] == 'pow' or tokens[0] == '**' or tokens[0] == 'exp':
-                    print reduce(power, numbers)
-                elif tokens[0] == 'mod' or tokens[0] == '%':
-                    print reduce(mod, numbers)
-                else:
-                    print "I don't understand"
-        except:
-            print "This doesn't work"
+        if calculate_from_line(user_input) == 'quit':
+            quit()
+        print calculate_from_line(user_input)
 
 if __name__ == '__main__':
     main()
