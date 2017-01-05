46be8803254138283793c05656f8300b4ce53467
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-04 16:15:53 -0800

    working calculator using reduce

--- calculator.py
+++ calculator.py
@@ -16,26 +16,28 @@ def main():
         try:
             if tokens[0] == 'q':
                 quit()
-            elif tokens[0] == '+':
-                print add(tokens[1:])
-            elif tokens[0] == '-':
-                print subtract(int(tokens[1]), int(tokens[2]))
-            elif tokens[0] == '*':
-                print multiply(int(tokens[1]), int(tokens[2]))
-            elif tokens[0] == '/':
-                print divide(int(tokens[1]), int(tokens[2]))
-            elif tokens[0] == 'square':
-                print square(int(tokens[1]))
-            elif tokens[0] == 'cube':
-                print cube(int(tokens[1]))
-            elif tokens[0] == 'pow' or tokens[0] == '**' or tokens[0] == 'exp':
-                print power(int(tokens[1]), int(tokens[2]))
-            elif tokens[0] == 'mod' or tokens[0] == '%':
-                print mod(int(tokens[1]), int(tokens[2]))
             else:
-                print "I don't understand"
+                numbers = map(int, tokens[1:])
+                if tokens[0] == '+':
+                    print reduce(add, numbers)
+                elif tokens[0] == '-':
+                    print reduce(subtract, numbers)
+                elif tokens[0] == '*':
+                    print reduce(multiply, numbers)
+                elif tokens[0] == '/':
+                    print reduce(divide, numbers)
+                elif tokens[0] == 'square':
+                    print square(int(tokens[1]))
+                elif tokens[0] == 'cube':
+                    print cube(int(tokens[1]))
+                elif tokens[0] == 'pow' or tokens[0] == '**' or tokens[0] == 'exp':
+                    print reduce(power, numbers)
+                elif tokens[0] == 'mod' or tokens[0] == '%':
+                    print reduce(mod, numbers)
+                else:
+                    print "I don't understand"
         except:
-            print "This doesn't work"        
+            print "This doesn't work"
 
 if __name__ == '__main__':
     main()
